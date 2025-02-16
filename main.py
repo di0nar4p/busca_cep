ffb65a1254582c393e698e9cfe17e9612ce95484
di0nar4p <glauco.santos@ng7consultoria.com.br>
2025-02-16 14:02:08 -0300

    Melhorias

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-from flask import Flask, request, jsonify, send_from_directory, render_template, redirect, flash, url_for
-from Controller.Requests import busca_cep
 import os
-
-
+from flask import (Flask, flash, redirect, render_template, request, send_from_directory, url_for)
+from Controller.Requests import busca_cep
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 @app.route('/cep', methods=['GET'])
 def index():
-        cep = request.args.get('cep')
-        if cep:
-            dados= busca_cep(cep)
-            if "erro" in dados.keys():
-                return redirect(url_for('erro'))
-            else:
-                return render_template('main.html', dados=dados)
-            
-        
 
+    cep = request.args.get('cep')
+    if cep:
+        dados= busca_cep(cep)
+        if "erro" in dados.keys():
+            return redirect(url_for('erro'))
+        else:
+            return render_template('main.html', dados=dados) 
+            
 @app.route('/erro', methods=["GET"])
 def erro():
+
     flash('CEP não encontrado!')
     return redirect(url_for('main'))
 
